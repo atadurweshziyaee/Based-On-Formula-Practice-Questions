37b78dd2e456b98a7d6b3648a3570e37e154cf8d
Ata <atadurweshziyaee@gmail.com>
2023-02-13 00:34:23 +0530

    Write a java prog to find the Area Of a Circle.

---
+++ AreaOfCircle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class AreaOfCircle {
+    public static void main(String[] args) {
+        // a = pi * r^2;
+        double radius;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius :");
+        radius = sc.nextDouble();
+
+        double area = Math.PI * radius * radius;
+        System.out.println("AREA OF A CIRCLE IS :- "+ area);
+    }
+}
