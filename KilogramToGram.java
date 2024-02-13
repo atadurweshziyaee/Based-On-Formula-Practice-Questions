4c9fa43fbd321b9446dd60ccaecf1ae797f8713d
Ata <atadurweshziyaee@gmail.com>
2024-02-13 21:36:06 +0530

    Kilogram to Gram.

---
+++ KilogramToGram.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class KilogramToGram {
+    public static void main(String args[]){
+
+        // taking input in meter.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Convert Kilogram (kg) to Gram (g) Enter:- ");
+        double kg = sc.nextDouble();
+
+        //calling mToMm() method.
+        double result = kgToG(kg);
+        System.out.println("========================================================");
+        System.out.println("\n\n Kilogram \t\t\t: "+kg+"kg\n           \t to");
+        System.out.println("Gram           \t\t: "+result+"g");
+        System.out.println("========================================================");
+    }
+
+    // it will convert meter to millimeter.
+    public static double kgToG(double kg){
+        double gram = kg * 1000.0d;
+        return gram;
+    }
+}
